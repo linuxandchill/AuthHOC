8343e327407f1621f8cda745ec6d8e0bbc8edf5c
ck <thecurrencykid@gmail.com>
2016-10-07 14:49:09 -0400

    life cycle methods added to kick back to / page

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -10,11 +10,19 @@ export default function(ComposedComponent){
     }
 
     componentWillMount(){
-      this.context.router.push('/'); 
+      if(!this.props.authenticated){
+        this.context.router.push('/'); 
+      }
+    }
+
+    componentWillUpdate(nextProps){
+      if(!nextProps.authenticated){
+        this.context.router.push('/'); 
+      }
     }
 
     render(){
-      console.log(this.context); 
+    // console.log(this.context); 
       return <ComposedComponent {...this.props} /> 
     }
   }
